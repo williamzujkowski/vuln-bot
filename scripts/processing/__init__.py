1ccc1eda754006c54513b1ccb8d123074e6a03ac
William Zujkowski <williamzujkowski@gmail.com>
2025-10-19 01:26:37 -0400

    fix: remove non-existent impact_scorer import to unblock harvest pipeline

--- scripts/processing/__init__.py
+++ scripts/processing/__init__.py
@@ -1,9 +1,7 @@
 """Data processing and normalization modules."""
 
-from scripts.processing.impact_scorer import ImpactScorer
 from scripts.processing.risk_scorer import RiskScorer
 
 __all__ = [
-    "ImpactScorer",
     "RiskScorer",
 ]
